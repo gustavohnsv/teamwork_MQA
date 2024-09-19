852a33a441b68e61b6d04a76a361194389c965a3
gustavohnsv <guvi10vieira@gmail.com>
2024-09-19 17:30:19 -0300

    fix: fixed setwd()
    - fixed setwd() fun with rstudioapi package

--- src/load.R
+++ src/load.R
@@ -1,23 +1,25 @@
-# Verifica se está no diretório correto para tentar ler o arquivo
-if (!(grepl("/Documentos/workspace/teamwork_MQA", getwd()))) {
-  setwd("Documentos/workspace/teamwork_MQA")  
-}
+# Biblioteca com algumas funções úteis do RStudio
+library(rstudioapi)
 
+# Definindo o diretório de trabalho para a localização da pasta "teamwork_MQA"
+setwd(dirname(dirname(getActiveDocumentContext()$path)))
+
+# Caminho para o arquivo onde contém as variáveis que serão utilizadas
 file_path <- "./data/trafico-escravos-variaveis-utilizadas.csv"
 
-if (!file.exists(file_path)) {
+if (!file.exists(file_path)) { # Verifica se existe um arquivo com o nome do "file_path"
   message("File not found with ", file_path)
 } else {
-  # Leitura dos dados brutos
-  brute_data <- tryCatch({
+  brute_data <- tryCatch({ # Leitura dos dados brutos
     read.csv("./data/trafico-escravos-variaveis-utilizadas.csv", header = TRUE)  
   }, error = function(e) {
     message("Error reading archive: ", e$message)
     NULL
   })
-  
-  if (!is.null(brute_data)) {
+  if (!is.null(brute_data)) { # Verifica se os dados foram lidos com sucesso
     message("File read succefully with ", file_path)
   }
-  
 }
+
+# Remove o caminho para o arquivo (não será mais necessário) 
+rm(file_path)
