0c599bc003278ced1b6bbd5d61281807ccaa01ad
gustavohnsv <guvi10vieira@gmail.com>
2024-10-03 21:15:40 -0300

    docs: some stuffs
    - added code comments
    - modified readme file

--- src/data.R
+++ src/data.R
@@ -1,7 +1,12 @@
+# Adiciona uma coluna indicando a cor do vinho de cada dataset
 white_wines$colour <- "white"
 red_wines$colour <- "red"
-wines <- rbind(white_wines, red_wines)
-wines <- na.omit(wines)
-wines$rating <- ""
 
+# Combina os datasets para um único
+wines <- rbind(white_wines, red_wines)
+
+# Retira possíveis observações com campos NA
+wines <- na.omit(wines)
+
+# Sortea 5000 observações para compor uma "amostra da amostra"
 wines_sample <- wines[sample(nrow(wines), size = 5000, replace = FALSE), ]
--- src/plot.R
+++ src/plot.R
@@ -1,7 +1,9 @@
+# Histograma com uma curva de densidade para a variável pH
 ggplot(wines, aes(x = pH)) +
   geom_histogram(aes(y = after_stat(density)), colour = 1, fill = "#FFFFFF", bins = 200) +
   geom_density(lwd = 1.2, linetype = 2, colour = "#FF69B4", fill = "#FF69B4", alpha = 0.25)
 
+# Gráfico de dispersão com uma linha de correlação para as variáveis pH e ácido cítrico
 ggplot(wines, aes(x = pH, y = citric.acid)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE)
