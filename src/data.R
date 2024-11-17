d1d303f9179e460b3c31fcd398cb8366eac604e2
GustavoBastos04 <gubasouza2004@gmail.com>
2024-11-16 21:46:55 -0300

    Add Example

--- src/data.R
+++ src/data.R
@@ -19,7 +19,7 @@ wines <- wines %>%
 wines$is.red <- as.factor(wines$is.red)
 
 # Sorteia 5000 observações para compor uma "amostra da amostra"
-wines_sample <- wines[sample(nrow(wines), size = 500, replace = FALSE), ]
+wines_sample <- wines[sample(nrow(wines), size = 5000, replace = FALSE), ]
 
 # Aplicando a padronização a cada coluna do dataframe
 wines_standardized <- as.data.frame(lapply(wines_sample, standardize_z_score))
@@ -97,6 +97,7 @@ matriz_confusao <- table(Real = wines_sample$is.red, Previsto = previsoes)
 # Visualize a matriz de confusão
 print(matriz_confusao)
 
+#Tabel de frequência para açúcar residual
 # Análise de Cluster: açúcar residual x densidade
 # Procedimento hierárquico
 # Distância Euclidiana entre cada objeto 
@@ -104,9 +105,9 @@ sugar_density_df <- data.frame(
   sugar = wines_standardized$residual.sugar,
   density = wines_standardized$density
 )
-# Retirada de outliers por quantis
+# Retirada de outliers por quantis (Apenas para procedimento não hierárquico)
 filtered_sugar_density_df <- na.omit(no_outliers_df(sugar_density_df))
-euclidian_dist <- dist(filtered_sugar_density_df)
+euclidian_dist <- dist(sugar_density_df)
 
 # Agrupamento hierárquico aglomerativo por single linkage
 hierarchical_groups <- hclust(euclidian_dist, method = "single")
@@ -119,7 +120,7 @@ plot(hierarchical_groups, main = "Dendrograma - Single Linkage", sub = "", xlab 
 nbclust_wss <- NbClust(filtered_sugar_density_df, distance = "euclidean", min.nc = 2, max.nc = 10, method = "kmeans", index = "silhouette")
 print(nbclust_wss$Best.nc)
 
-k <- 4 # Número ideal de clusters dado
+k <- 4 # Número ideal de clusters dado pelo método da silhueta
 non_hierarchical_kmeans <- kmeans(filtered_sugar_density_df, centers = k, nstart = 25)
 
 # Clusters atribuídos
@@ -136,3 +137,46 @@ non_hierarchical_cluster_data <- data.frame(filtered_sugar_density_df, Cluster =
 ggplot(non_hierarchical_cluster_data, aes(x = non_hierarchical_cluster_data$sugar, y = non_hierarchical_cluster_data$density, color = Cluster)) +
   geom_point() +
   theme_minimal()
+
+# Exemplo análise de Cluster
+# Data Frame
+data("mtcars")
+View(mtcars)
+
+mtcars_summ <- mtcars[,c(1,2)]
+
+# Z-Score (padronização)
+mtcars_summ_stand <- as.data.frame(lapply(mtcars_summ, standardize_z_score))
+
+# Distância Euclidiana 
+mtcars_euclidian_dist <-  dist(mtcars_summ_stand)
+
+# Agrupamento hierárquico aglomerativo por single linkage
+mtcars_hierarchical_groups <- hclust(mtcars_euclidian_dist, method = "single")
+mtcars_hierarchical_groups$labels <- rownames(mtcars)
+# Dendograma
+plot(mtcars_hierarchical_groups, main = "Dendrograma - mpg & cyl",sub = "", xlab = "", ylab= "Distance")
+
+# Procedimento não hierárquico
+# Retirada de outliers
+filtered_mtcars_summ <- na.omit(no_outliers_df(mtcars_summ))
+
+#Método da silhueta para encontrar k ideal
+mt_cars_silhou <- NbClust(filtered_mtcars_summ, 
+                          distance = "euclidean", 
+                          min.nc = 2, max.nc = 10, method = "kmeans", 
+                          index = "silhouette")
+print(mt_cars_silhou$Best.nc)
+mtcars_k <- 10 # Número ideal de clusters dado pelo método da silhueta
+mtcars_kmeans <- kmeans(filtered_mtcars_summ, centers = mtcars_k, nstart = 25)
+
+# Clusters atribuídos
+mtcars_kmeans$cluster
+# Tamanhos dos clusters
+mtcars_kmeans$size
+# Centros dos clusters
+mtcars_kmeans$centers
+
+# Visualização dos dados clusterizados
+mtcars_mean_data <- data.frame(filtered_mtcars_summ, Cluster = as.factor(mtcars_kmeans$cluster))
+ggplot(mtcars_mean_data, aes(x = cyl, y = mpg, color = Cluster)) + geom_point() + theme_minimal()
